1e25d49055112e9c29cb5a491681f6e402854f86
camisavines <camisa.vines@icloud.com>
2022-01-28 17:40:17 -0500

    new setup

---
+++ src/app-router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function AppRouter() {
+  return (
+    <div className="App">
+      <h1>Home page</h1>
+    </div>
+  );
+}
+
+export default AppRouter;
